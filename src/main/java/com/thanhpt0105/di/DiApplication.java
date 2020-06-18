a43b0fc3ca577acd95708919b7568e65e9dd1f6b
thanhpt0105 <thanhpt0105@gmail.com>
2020-06-18 10:51:35 +0800

    Property Source example

--- src/main/java/com/thanhpt0105/di/DiApplication.java
+++ src/main/java/com/thanhpt0105/di/DiApplication.java
@@ -1,6 +1,7 @@
 package com.thanhpt0105.di;
 
 import com.thanhpt0105.di.controllers.*;
+import com.thanhpt0105.di.examplebeans.FakeDataSource;
 import jdk.nashorn.internal.objects.annotations.Setter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,22 +17,11 @@ public class DiApplication {
 
         MyController myController = (MyController) applicationContext.getBean("myController");
 
-        System.out.println(myController.getGreeting());
+        FakeDataSource fakeDataSource = (FakeDataSource) applicationContext.getBean(FakeDataSource.class);
 
-
-        //Add annotation @Controller and @Service to the controllers and service so Spring knows them as beans and initiate
-        //Add @Autowired for service declaration in property and setter Injection controller so Spring knows to inject the service.
-        //Constructor injection controller doesn't need to use Autowire annotation.
-//        PropertyInjectedController propertyInjectedController = (PropertyInjectedController) applicationContext.getBean("propertyInjectedController");
-//        SetterInjectedController setterInjectedController = (SetterInjectedController) applicationContext.getBean("setterInjectedController");
-//        ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) applicationContext.getBean("constructorInjectedController");
-//
-//        System.out.println(propertyInjectedController.getGreeting());
-//        System.out.println(setterInjectedController.getGreeting());
-//        System.out.println(constructorInjectedController.getGreeting());
-
-//        I18nController i18nController = (I18nController) applicationContext.getBean("i18nController");
-//        System.out.println(i18nController.getGreeting());
+        System.out.println(fakeDataSource.getUsername());
+        System.out.println(fakeDataSource.getPassword());
+        System.out.println(fakeDataSource.getDburl());
     }
 
 }
---
+++ src/main/java/com/thanhpt0105/di/config/PropertyConfig.java
@@ -0,0 +1,37 @@
+package com.thanhpt0105.di.config;
+
+import com.thanhpt0105.di.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+
+    @Value("${ds.username}")
+    String user;
+
+    @Value("${ds.password}")
+    String password;
+
+    @Value("${ds.dburl}")
+    String dburl;
+
+    @Bean
+    public FakeDataSource dataSource() {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUsername(user);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setDburl(dburl);
+        return fakeDataSource;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties () {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
